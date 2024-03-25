a76c0fa5cbafa9025f26e7635518629243501ae9
Pratham216 <prathambisht282@gmail.com>
2024-03-25 19:35:51 +0530

    Expression java

---
+++ Exercise.java/Expression.java
@@ -0,0 +1,10 @@
+public class Expression {
+    public static void main(String[] args) {
+        int a=10,b=20,c=30,x=40,y=50,z=60;
+        int ans1 = (a/c)*(z/a);
+        int ans2 = (c++)+(b/x)-y;
+
+        System.out.println(ans1);
+        System.out.println(ans2);
+    }
+}
