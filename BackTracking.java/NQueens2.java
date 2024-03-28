911be5066c5b1469f6d54f79659d07ed4807b5dc
Pratham216 <prathambisht282@gmail.com>
2024-03-28 10:22:19 +0530

    N Queen only one solution

---
+++ BackTracking.java/NQueens2.java
@@ -0,0 +1,75 @@
+    public class NQueens2 {
+
+
+        public static boolean isSafe(char board[][], int row, int col) {
+            // vertical up
+            for (int i = row - 1; i >= 0; i--) {
+                if (board[i][col] == 'Q') {
+                    return false;
+                }
+            }
+
+            // diagonal left
+            for (int i = row - 1, j = col - 1; i >= 0 && j >= 0; i--, j--) {
+                if (board[i][j] == 'Q') {
+                    return false;
+                }
+            }
+
+            // diagonal right
+            for (int i = row - 1, j = col + 1; i >= 0 && j<board.length; i--, j++) {
+                if (board[i][j] == 'Q') {
+                    return false;
+                }
+            }
+
+            return true;
+
+        }
+
+        public static boolean nQueens(char board[][], int row) {
+            // base case
+            if (row == board.length) {
+                return true;
+            }
+
+            for (int j = 0; j < board.length; j++) {
+                if (isSafe(board, row, j)) {
+                    board[row][j] = 'Q';
+                if (nQueens(board, row + 1)) {
+                    return true;
+                } 
+                    board[row][j] = 'x';
+                }
+            }
+            return false;
+        }
+
+        public static void print(char board[][]) {
+            System.out.println("--------------Chess Board----------");
+            for (int i = 0; i < board.length; i++) {
+                for (int j = 0; j < board.length; j++) {
+                    System.out.print(board[i][j] + " ");
+                }
+                System.out.println();
+            }
+        }
+
+        public static void main(String[] args) {
+            int n =2;
+            char board[][] = new char[n][n];
+
+            //initialise the chess board
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < n; j++) {
+                    board[i][j] = 'x';
+                }
+            }
+            if (nQueens(board, 0)) {
+                System.out.println("Solution is possible ");
+                print(board);
+            }else{
+            System.out.println("Solution is not possible");
+        }
+        }
+    }
